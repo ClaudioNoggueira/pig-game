45caa007825a9cf5f8fbed136807bebb4519dfe7
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-06-16 13:24:52 -0300

    Feature: Roll dice

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-const randomNumber = Math.floor(Math.random() * 6) + 1;
-console.log(`Random number: ${randomNumber}`);
+let randomNumber = 1;
 
+// ELEMENTS
 const diceImageEl = document.querySelector(`#diceImage`);
-diceImageEl.setAttribute(`src`, `images/dice-${randomNumber}.png`);
+const btnRollEl = document.querySelector(`#rollDice`);
+
+function rollDice() {
+  randomNumber = Math.floor(Math.random() * 6) + 1;
+  console.log(`Random number: ${randomNumber}`);
+
+  diceImageEl.setAttribute(`src`, `images/dice-${randomNumber}.png`);
+}
+
+window.onload = rollDice();
+
+btnRollEl.addEventListener(`click`, () => {
+  rollDice();
+});
